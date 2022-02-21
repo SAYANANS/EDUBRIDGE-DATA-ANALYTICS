8bd01fbb67cd40a5a4ad9885745e904e7a650fca
SAYANA NS <98824522+SAYANANS@users.noreply.github.com>
2022-02-21 12:46:12 +0530

    initial commit

---
+++ Assignments/python/python assignment-7.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## first character in string
+
+# In[1]:
+
+
+s="hello sayana"
+print(s[0])
+
+
+# ## Last character in string using len function
+
+# In[3]:
+
+
+s="Good Morning"
+length=len(s)
+last_char=s[length -1]
+print(last_char)
+
+
+# ## Last character in string
+
+# In[4]:
+
+
+s="congragulations"
+print(s[-1])
+
+
+# ## Fetch the 7th element of the string
+
+# In[5]:
+
+
+s="Best wishes"
+print(s[6])
+
+
+# ## Fetch all characters from 0 to 5 index location excluding the character at loc 5
+
+# In[9]:
+
+
+s="Best wishes"
+print(s[0:5])
+
+
+# ## Retrieve all characters between 6-12 index location excluding loc 12
+
+# In[10]:
+
+
+s="All the best for your exam"
+print(s[6:12])
+
+
+# ## Retrieve last four characters of the string
+
+# In[17]:
+
+
+s="All the best for your exam"
+last=s[-4:]
+print(last)
+
+
+# ## Retrieve last six characters of the string
+
+# In[18]:
+
+
+s="All the best for your exam"
+last=s[-6:]
+print(last)
+
+
+# ## Retrieve first four characters of the string
+
+# In[19]:
+
+
+s="All the best for your exam"
+first=s[0:5]
+print(first)
+
+
+# In[ ]:
+
+
+
+
