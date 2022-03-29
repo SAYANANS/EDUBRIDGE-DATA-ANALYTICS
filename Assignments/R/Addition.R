86ff5b46b74cb2327544b024abae9fa6d51c945c
SAYANA NS <98824522+SAYANANS@users.noreply.github.com>
2022-03-29 12:46:55 +0530

    initial commit

---
+++ Assignments/R/Addition.R
@@ -0,0 +1,6 @@
+num1<-readline(prompt="Enter the first number")
+num2<-readline(prompt="Enter the second number")
+num1<-as.integer(num1)
+num2<-as.integer(num2)
+res<-num1+num2
+print(paste("Result is",res))
